7fd9d6b8fef2d31697e81d2443120dee006cd4ca
Avilash Ravi <avilash17@gmail.com>
2023-09-20 15:44:11 +0530

    2nd commit with docx download and preview content

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, send_file, session, url_for
+from flask_session import Session
 import os
 import fitz  # PyMuPDF
 from PIL import Image
 import pytesseract
 from langdetect import detect, lang_detect_exception
+from googletrans import Translator
+from docx import Document
+import secrets  # Import the secrets module to generate a secret key
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)  # Generate a random secret key
+app.config['SESSION_TYPE'] = 'filesystem'  # Use the filesystem to store session data
+Session(app)
 
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg'}
@@ -33,40 +40,80 @@ def detect_language(text):
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
-            return jsonify({'error': 'No file part'})
+            return render_template('upload.html', error='No file part')
 
         file = request.files['file']
 
         if file.filename == '':
-            return jsonify({'error': 'No selected file'})
+            return render_template('upload.html', error='No selected file')
 
         if file and allowed_file(file.filename):
             filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
             file.save(filename)
 
-            file_type = "PDF" if filename.endswith('.pdf') else "Image" if filename.endswith(('.png', '.jpg','.jpeg')) else "Text"
+            file_type = "PDF" if filename.endswith('.pdf') else "Image" if filename.endswith(('.png', '.jpg', '.jpeg')) else "Text"
             detected_language = None
+            translated_text = None
 
             if file_type == "PDF":
                 pdf_text = extract_text_from_pdf(filename)
                 detected_language = detect_language(pdf_text)
-
+                if detected_language != 'ta':
+                    translator = Translator()
+                    translation = translator.translate(pdf_text, src=detected_language, dest='ta')
+                    if translation:
+                        translated_text = translation.text
+                    else:
+                        translated_text = "Translation failed"
+                else:
+                    translated_text = pdf_text
             elif file_type == "Image":
                 img = Image.open(filename)
                 text_en_hi = pytesseract.image_to_string(img, lang='eng+hin')
                 text_ta = pytesseract.image_to_string(img, lang='tam')
                 extracted_text = text_en_hi + " " + text_ta
                 detected_language = detect_language(extracted_text)
-
+                if detected_language != 'ta':
+                    translator = Translator()
+                    translation = translator.translate(extracted_text, src=detected_language, dest='ta')
+                    if translation:
+                        translated_text = translation.text
+                    else:
+                        translated_text = "Translation failed"
+                else:
+                    translated_text = extracted_text
             else:
                 with open(filename, 'r', encoding='utf-8') as text_file:
                     file_content = text_file.read()
                 detected_language = detect_language(file_content)
+                if detected_language != 'ta':
+                    translator = Translator()
+                    translation = translator.translate(file_content, src=detected_language, dest='ta')
+                    if translation:
+                        translated_text = translation.text
+                    else:
+                        translated_text = "Translation failed"
+                else:
+                    translated_text = file_content
 
-            return render_template('upload.html', file_type=file_type, detected_language=detected_language)
+            # Create and save the translated DOCX file
+            doc = Document()
+            doc.add_paragraph(translated_text)
+            docx_filename = 'translated_text.docx'
+            doc.save(docx_filename)
+
+            # Prepare the information to be displayed on the page
+            info_dict = {
+                'file_type': file_type,
+                'detected_language': language_names.get(detected_language, 'Unknown'),
+                'translated_text': translated_text,
+            }
+
+            # Serve the DOCX file for download and pass the info_dict to the template
+            return render_template('result.html', info=info_dict, docx_filename=docx_filename)
 
         else:
-            return jsonify({'error': 'File type not allowed'})
+            return render_template('upload.html', error='File type not allowed')
 
     return render_template('upload.html')
 
@@ -99,6 +146,10 @@ def extract_text_from_pdf(filename):
 
     pdf_document.close()
     return text
+
+@app.route('/download_docx/<filename>')
+def download_docx(filename):
+    return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
